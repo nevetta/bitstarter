87fd3728397e331bda8d4c170e3ffa3d97d97601
nevetta <nevetta@gmail.com>
2013-07-09 16:34:24 +0000

    updated web.js

--- web.js
+++ web.js
@@ -1,11 +1,11 @@
 var express = require('express');
+var fs = require('fs');
+var index = fs.readFileSync('index.html');
 
-var app = express.createServer(express.logger());
-
-fs.readFile('/index.html', function(err,data){
-  if(err) throw err;
-console.log(data);
+express.createServer(function (request, response) {
+  response.send(index);
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
