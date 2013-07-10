df2b0ec323880ef97ecf831b38ebfe0a1bbc4589
nevetta <nevetta@gmail.com>
2013-07-10 16:28:45 +0000

    updated

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
-var app=express.createServer(express.logger());
-app.get('/',function(request,response){
-response.send('hello world 2 !');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  var fs = require('fs');
+  var buffer = fs.readFileSync("indexa.html");
+  response.send(buffer.toString('utf8',0));
 });
+
 var port = process.env.PORT || 5000;
-app.listen(port,function(){
-console.log("Listening on"+port);
+app.listen(port, function() {
+  console.log("Listening on " + port);
 });
