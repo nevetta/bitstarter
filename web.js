e0145879be1b2b2b6d1429c36cae7feb80b3d719
nevetta <nevetta@gmail.com>
2013-07-10 11:03:15 +0000

    updated index.html

--- web.js
+++ web.js
@@ -1,11 +1,9 @@
-//var buffer = new Buffer(100);
-//buffer=fs.readFileSync('index.html');
-//console.log(buffer.toString('utf8',0,buffer.length);
-var text = fs.readFileSync('index.html','utf8');
-console.log(text);
-
-
+var express = require('express');
+var app=express.createServer(express.logger());
+app.get('/',function(request,response){
+response.send('hello world 2 !');
+});
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+app.listen(port,function(){
+console.log("Listening on"+port);
 });
